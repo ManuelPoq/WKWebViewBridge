5162e4eacb3dde2bf227f517e6b85d00313a8173
Manuel Marcos Regalado <manuel@poqstudio.com>
2018-11-15 15:31:12 +0000

    Add script and button

--- WKWebViewBridge/ViewController.swift
+++ WKWebViewBridge/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
     
     var webView: WKWebView?
+    var changeColorButton: UIButton?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,11 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
         webViewConfiguration.userContentController = userContentController
         
         
+        // Set up call to web
+        var userScript = WKUserScript(source: "setupApplePayButton()", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        userContentController.addUserScript(userScript)
+        
+        
         // Webview setup
         webView = WKWebView(frame: self.view.frame, configuration: webViewConfiguration)
         view.addSubview(webView!)
@@ -31,7 +37,17 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
         // Load the website
         let url = Bundle.main.url(forResource: "index", withExtension: "html")!
         webView?.loadFileURL(url, allowingReadAccessTo: url)
-   
+        
+        // Add Native button
+        changeColorButton = UIButton()
+        if let button = changeColorButton {
+            button.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(button)
+            button.setTitle("Change Web Button Color", for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50).isActive = true
+            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        }
     }
 
     // MARK: - WKScriptMessageHandler
@@ -47,6 +63,8 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
         self.present(alert, animated: true)
     }
-    
-}
 
+    @IBAction func changeColorButtonTapped(_ sender: Any) {
+        
+    }
+}
