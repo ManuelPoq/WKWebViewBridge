c34141ded376b6a5b1fcc6da82f0b5ca03e3daae
Manuel Marcos Regalado <manuel@poqstudio.com>
2018-11-15 14:22:17 +0000

    Improve code with comments and naming

--- WKWebViewBridge/ViewController.swift
+++ WKWebViewBridge/ViewController.swift
@@ -11,24 +11,24 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
     
-    
     var webView: WKWebView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set up for call backs from web
         let userContentController = WKUserContentController()
-        userContentController.add(self, name: "poqAppCallBack")
-        
+        userContentController.add(self, name: "poqAppCallBackFromWeb")
         let webViewConfiguration = WKWebViewConfiguration()
         webViewConfiguration.userContentController = userContentController
         
+        
+        // Webview setup
         webView = WKWebView(frame: self.view.frame, configuration: webViewConfiguration)
-       
         view.addSubview(webView!)
-
         webView?.navigationDelegate = self
         
+        // Load the website
         let url = Bundle.main.url(forResource: "index", withExtension: "html")!
         webView?.loadFileURL(url, allowingReadAccessTo: url)
    
@@ -36,12 +36,12 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
 
     // MARK: - WKScriptMessageHandler
     
+    // Handles the call backs from the web
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         
         guard let body = message.body as? String else {
             return
         }
-        
         
         let alert = UIAlertController(title: "Message From Web", message: body.removingPercentEncoding, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
